d3646be1c4811023e51fcbb9132b9aa759398100
Franco Colmenarez <francoacg1@gmail.com>
2019-01-11 21:19:12 -0400

    Drawing a square in a window with the Turtle module

---
+++ turtle_screen.py
@@ -0,0 +1,18 @@
+import turtle
+
+window = turtle.Screen()
+tortuga = turtle.Turtle()
+
+turtle.forward(100)
+turtle.right(90)
+
+turtle.forward(100)
+turtle.right(90)
+
+turtle.forward(100)
+turtle.right(90)
+
+turtle.forward(100)
+turtle.right(90)
+
+window.mainloop()
